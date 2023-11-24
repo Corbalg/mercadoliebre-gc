74fae595b002a38da4b43a5cdd697551375405b0
Corbalg <corbalg@gmail.com>
2023-11-23 22:09:23 -0300

    creacion carpeta src

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-    console.log('Servidor corriendo en puerto http://localhost:${port}');
+    console.log("Servidor corriendo en puerto http://localhost:${port}");
 })
                   
 app.get('/', (req, res) => {
